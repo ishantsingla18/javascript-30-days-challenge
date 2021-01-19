5156d604057d089c3cad69364838a4ad6d6767af
ishantsingla18 <57613954+ishantsingla18@users.noreply.github.com>
2021-01-19 23:19:37 +0530

    Project 12

---
+++ Project(12)_faq/script.js
@@ -0,0 +1,8 @@
+const buttons = document.querySelectorAll('.btn');
+
+buttons.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        btn.parentNode.classList.toggle('active');
+    })
+})
+
